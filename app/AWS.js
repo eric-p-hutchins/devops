54be852e7ad33312b720572516b4c3525d5008ac
hutchiep190 <eric.hutchins@fundbase.com>
2017-02-02 10:40:19 -0500

    Replace AWS CLI with NodeJS aws-sdk

--- app/AWS.js
+++ app/AWS.js
@@ -1,7 +1,7 @@
+const AWS = require('aws-sdk')
 const awsImageId = 'ami-0b33d91d'
 const awsSecurityGroupIds = 'sg-a86ad7d4'
 const awsKeyName = 'eph-key-pair'
-const awsRegion = 'us-east-1'
 const awsInstanceType = 't2.micro'
 const exec = require('child_process').exec
 
@@ -9,11 +9,21 @@ function startInstance() {
   // Generate a random number in the range 0 - 9999
   const dockerContainerIds = Math.floor(Math.random() * 10000)
 
-  exec(`aws ec2 run-instances --image-id ${awsImageId} --security-group-ids ${awsSecurityGroupIds} --count 1 --instance-type ${awsInstanceType} --region ${awsRegion} --key-name ${awsKeyName} --user-data ${dockerContainerIds}`, function(error, stdout, stderr) {
-    if (error) {
-      return console.log('Error starting instance', error)
+  var ec2 = new AWS.EC2()
+  var params = {
+    ImageId: awsImageId,
+    SecurityGroupIds: [awsSecurityGroupIds],
+    MinCount: 1,
+    MaxCount: 1,
+    InstanceType: awsInstanceType,
+    KeyName: awsKeyName,
+    UserData: dockerContainerIds.toString()
+  }
+  ec2.runInstances(params, (err, data) => {
+    if (err) {
+      return console.log('Error starting instance', err, err.stack)
     } else {
-      console.log(stdout)
+      console.log(data)
     }
   })
 }
